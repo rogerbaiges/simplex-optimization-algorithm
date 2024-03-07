ac581ec676f7b116a1c87b0e754b91e93e8bee98
caiselvass <caiselvassala@gmail.com>
2024-03-07 17:04:47 +0100

    ADD structure of the classes

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from simplex_class import Simplex
+from problem_class import Problem
+import numpy as np
+
+if __name__ == '__main__':
+	problems_ids = [(10, 1), (10, 2), (10, 3), (10, 4), (48, 1), (48, 2), (48, 3), (48, 4)]
+	problems = [Problem(data_id=data_id, problem_id=problem_id) for data_id, problem_id in problems_ids]
+	
+	for problem in problems:
+		simplex = Simplex(problem=problem)
+		simplex.solve()
+
 
