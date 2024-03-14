6d747c61cf76212310b2217552d36e9cb2f4b4de
caiselvass <caiselvassala@gmail.com>
2024-03-14 20:02:44 +0100

    MOD useless

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from problem_class import Problem
 import numpy as np
 
 if __name__ == '__main__':
-	problems_ids = [(10, 1), (10, 2), (10, 3), (10, 4), (48, 1), (48, 2), (48, 3), (48, 4)]
+	problems_ids = [(10, 1), (10, 2), (10, 3), (10, 4), 
+				 	(48, 1), (48, 2), (48, 3), (48, 4)]
 	problems = [Problem(data_id=data_id, problem_id=problem_id) for data_id, problem_id in problems_ids]
+	simplex = Simplex(print_results=True, print_iters=False, save_results=False)
+
+	p = Problem(data_id=1, problem_id=4)
+	simplex.solve(p)
